1ea72bf48daa22b203056fbcb731e9c48de51891
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-11-02 19:25:48 +0530

    Add files via upload

--- Day1_Project.py
+++ Day1_Project.py
@@ -6,4 +6,3 @@ city = input("Enter the name of your city you grew up: \n")
 pet = input("Enter the name of your pet you love:\n")
 #4. Combine the name of their city and pet and show them their band name.
 print("Your brand name is: " + city + " " + pet)
-#5. Make sure the input cursor shows on a new line:
